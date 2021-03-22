08b69d7dccf069b6fe6627db04b3d0f611426481
h.khaidarov <h.khaidarov@qt.media>
2021-03-23 01:58:58 +0300

    server: add gracefull shutdown; add sync write

--- server/server.go
+++ server/server.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"io"
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 type req struct {
 	data []byte
-	resp chan bool
+	out  chan bool
 }
 
 type logger struct {
 	file *os.File
-	reqs chan *req
+	in   chan *req
+	done chan bool
 }
 
 func newLogger(path string) (*logger, error) {
@@ -24,41 +30,102 @@ func newLogger(path string) (*logger, error) {
 	}
 	return &logger{
 		file: file,
-		reqs: make(chan *req),
+		in:   make(chan *req),
+		done: make(chan bool),
 	}, nil
 }
 
 func (lgr *logger) log(data []byte) error {
 	_, err := lgr.file.Write(data)
+	if err == nil {
+		err = lgr.file.Sync()
+	}
 	return err
 }
 
 func (lgr *logger) run() {
-	for req := range lgr.reqs {
+	for req := range lgr.in {
 		err := lgr.log(req.data)
 		if err != nil {
-			log.Println("failed to log block")
+			log.Println("failed to write block")
 		}
-		req.resp <- err == nil
+		req.out <- err == nil
+	}
+	lgr.done <- true
+}
+
+type acceptor struct {
+	ln net.Listener
+	q  chan *req
+	wg sync.WaitGroup
+}
+
+func newAcceptor(addr string, q chan *req) (*acceptor, error) {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return &acceptor{
+		ln: ln,
+		q:  q,
+	}, nil
+}
+
+func (acc *acceptor) run() {
+	defer acc.wg.Done()
+	acc.wg.Add(1)
+	quit := false
+
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		<-sigs
+		log.Println("shutting down...")
+		signal.Stop(sigs)
+
+		quit = true
+		acc.ln.Close()
+		acc.wg.Wait()
+		close(acc.q)
+	}()
+
+	for {
+		conn, err := acc.ln.Accept()
+		if quit {
+			break
+		}
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		log.Println("accepted")
+		acc.wg.Add(1)
+		go acc.handleClient(conn)
 	}
 }
 
-func handleClient(conn net.Conn, reqs chan *req) {
+func (acc *acceptor) handleClient(conn net.Conn) {
+	defer acc.wg.Done()
 	defer conn.Close()
+
+	writer := bufio.NewWriter(conn)
 	data := make([]byte, 10)
 	resp := make(chan bool)
 
 	for {
 		read, err := conn.Read(data)
 		if read != 0 {
-			reqs <- &req{
+			acc.q <- &req{
 				data: data,
-				resp: resp,
+				out:  resp,
 			}
 			if <-resp == false {
 				break
 			}
-			_, err := conn.Write([]byte("ok\n"))
+			err := writer.WriteByte(byte(1))
+			if err == nil {
+				err = writer.Flush()
+			}
 			if err != nil {
 				log.Println("failed to write client")
 				break
@@ -67,30 +134,38 @@ func handleClient(conn net.Conn, reqs chan *req) {
 		if err != nil {
 			if err != io.EOF {
 				log.Println("failed to read client")
+			} else {
+				log.Println("conn is closed")
 			}
 			break
 		}
 	}
 }
 
+var trace *bool
+var logfile *string
+
 func main() {
-	lgr, err := newLogger("/tmp/test.xlog")
+	trace = flag.Bool("v", false, "enable tracing")
+	logfile = flag.String("f", "test.xlog", "path to xlog")
+	flag.Parse()
+
+	addr := ":8080"
+	if flag.NArg() != 0 {
+		addr = flag.Args()[0]
+	}
+
+	lgr, err := newLogger(*logfile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ln, err := net.Listen("tcp", ":8080")
+	acc, err := newAcceptor(addr, lgr.in)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	go lgr.run()
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		go handleClient(conn, lgr.reqs)
-	}
+	go acc.run()
+	<-lgr.done
 }
